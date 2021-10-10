5c7eb1565a8a6a896dad739098ede114cfee3baf
kursatsonmez <kursatsonmez9@hotmail.com>
2021-10-10 15:26:46 +0300

    Add project files.

---
+++ TempUnitTest/Tests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace TempUnitTest
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var res = 2 * 2;
+            Assert.Equal(4, res);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var res = 2 + 2;
+            Assert.Equal(4, res);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var res = 2 + 2;
+            Assert.Equal(4, res);
+        }
+    }
+}
